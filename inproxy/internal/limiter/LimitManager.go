eb6525bc6c7a486e090317957bc1dc1899287a01
kingson4wu <kingson4wu@gmail.com>
2023-02-27 18:07:47 +0800

    ratelimit

--- inproxy/internal/limiter/LimitManager.go
+++ inproxy/internal/limiter/LimitManager.go
@@ -1,21 +1,39 @@
 package limiter
 
-import "golang.org/x/time/rate"
+import (
+	"golang.org/x/time/rate"
+	"sync"
+)
 
 var limitMap map[string]*rate.Limiter
+var keyLocks sync.Map
 
 func init() {
-	//TODO 流量控制
-	//https://cloud.tencent.com/developer/article/1847918
-	//limiter := rate.NewLimiter(10, 100)
+	// https://cloud.tencent.com/developer/article/1847918
+	// limiter := rate.NewLimiter(10, 100)
+
+	limitMap = make(map[string]*rate.Limiter)
 }
 
 func IsLimit(serviceName string, uri string) bool {
+	key := serviceName + "_" + uri
 
-	limiter, ok := limitMap[serviceName+"_"+uri]
+	limiter, ok := limitMap[key]
 	if ok {
 		return limiter.Allow()
 	}
-	return false
 
+	keyLock, _ := keyLocks.LoadOrStore(key, &sync.Mutex{})
+	keyLock.(*sync.Mutex).Lock()
+	defer keyLock.(*sync.Mutex).Unlock()
+
+	limiter, ok = limitMap[key]
+	if ok {
+		return limiter.Allow()
+	}
+	qps := 10
+	limiter = rate.NewLimiter(rate.Limit(qps), qps)
+	limitMap[key] = limiter
+
+	return limiter.Allow()
 }
